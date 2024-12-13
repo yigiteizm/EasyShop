24f1db9be47065232752770acae52f54d7b02d08
Yigit <yigistaff@gmail.com>
2024-12-13 14:59:07 -0500

    getAllCategories method has been added to MysqlCategoryDao.

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,69 +1,87 @@
-package org.yearup.data.mysql;
+    package org.yearup.data.mysql;
 
-import org.springframework.stereotype.Component;
-import org.yearup.data.CategoryDao;
-import org.yearup.models.Category;
+    import org.springframework.stereotype.Component;
+    import org.yearup.data.CategoryDao;
+    import org.yearup.models.Category;
 
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+    import javax.sql.DataSource;
+    import java.sql.Connection;
+    import java.sql.PreparedStatement;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
+    import java.util.ArrayList;
+    import java.util.List;
 
-@Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
+    @Component
+    public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     {
-        super(dataSource);
+        public MySqlCategoryDao(DataSource dataSource)
+        {
+            super(dataSource);
+        }
+
+        @Override
+        public List<Category> getAllCategories() {
+            List<Category> categories = new ArrayList<>();
+            String sql = "SELECT * FROM categories";
+
+            try (Connection connection = getConnection();
+                 PreparedStatement statement = connection.prepareStatement(sql);
+                 ResultSet resultSet = statement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    categories.add(mapRow(resultSet));
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException("Error retrieving categories", e);
+            }
+
+            return categories;
+        }
+
+
+
+        @Override
+        public Category getById(int categoryId)
+        {
+            // get category by id
+            return null;
+        }
+
+        @Override
+        public Category create(Category category)
+        {
+            // create a new category
+            return null;
+        }
+
+        @Override
+        public void update(int categoryId, Category category)
+        {
+            // update category
+        }
+
+        @Override
+        public void delete(int categoryId)
+        {
+            // delete category
+        }
+
+        private Category mapRow(ResultSet row) throws SQLException
+        {
+            int categoryId = row.getInt("category_id");
+            String name = row.getString("name");
+            String description = row.getString("description");
+
+            Category category = new Category()
+            {{
+                setCategoryId(categoryId);
+                setName(name);
+                setDescription(description);
+            }};
+
+            return category;
+        }
+
     }
-
-    @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
-    }
-
-    @Override
-    public Category getById(int categoryId)
-    {
-        // get category by id
-        return null;
-    }
-
-    @Override
-    public Category create(Category category)
-    {
-        // create a new category
-        return null;
-    }
-
-    @Override
-    public void update(int categoryId, Category category)
-    {
-        // update category
-    }
-
-    @Override
-    public void delete(int categoryId)
-    {
-        // delete category
-    }
-
-    private Category mapRow(ResultSet row) throws SQLException
-    {
-        int categoryId = row.getInt("category_id");
-        String name = row.getString("name");
-        String description = row.getString("description");
-
-        Category category = new Category()
-        {{
-            setCategoryId(categoryId);
-            setName(name);
-            setDescription(description);
-        }};
-
-        return category;
-    }
-
-}
