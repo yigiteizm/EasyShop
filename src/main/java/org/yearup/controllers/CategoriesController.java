1a70da4df272652ce655cc01b6b7f01aedc61bd0
Yigit <yigistaff@gmail.com>
2024-12-16 11:39:54 -0500

    Listing has been added to CategoriesController.

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,7 +1,9 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -27,10 +29,14 @@ public class CategoriesController
         this.productDao = productDao;
 
     }
+    @GetMapping
     public List<Category> getAll()
     {
-        // find and return all categories
-        return null;
+        try {
+            return categoryDao.getAllCategories();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error listing categories.");
+        }
     }
 
     // add the appropriate annotation for a get action
