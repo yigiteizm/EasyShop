6f6219f5d376a6b5dc7d4c2884a4caf24051720c
Yigit <yigistaff@gmail.com>
2024-12-16 13:35:25 -0500

    Bug 1 found:minimum price range has been added to filter method in MydqlProductDao.

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -25,8 +25,9 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
 
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (color = ? OR ? = '') ";
+                "AND (price >= ? OR ? = -1) " +      // #Query has been corrected: fixed price range minPrice has been added.
+                "AND (price <= ? OR ? = -1) " +
+                "AND (color = ? OR ? = '')";
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
