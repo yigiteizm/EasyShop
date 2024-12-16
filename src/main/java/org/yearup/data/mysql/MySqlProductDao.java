a72fdc2e4f49b9461815fc429e13715070b572af
Yigit <yigistaff@gmail.com>
2024-12-16 13:41:37 -0500

    Bug 1 has been fixed  corrected price in product search query

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -25,7 +25,7 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
 
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
-                "AND (price >= ? OR ? = -1) " +      // #Query has been corrected: fixed price range minPrice has been added.
+                "AND (price >= ? OR ? = -1) " +      // #Bug 1Query has been corrected: fixed price range minPrice has been added.
                 "AND (price <= ? OR ? = -1) " +
                 "AND (color = ? OR ? = '')";
 
@@ -41,8 +41,11 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, color);
-            statement.setString(6, color);
+            statement.setBigDecimal(5, maxPrice);  // #Bug 1: price <= maxPrice has been implemented.
+            statement.setBigDecimal(6, maxPrice);  // #Bug 1: price <= maxPrice has been implemented.
+            statement.setString(7, color);
+            statement.setString(8, color);
+
 
             ResultSet row = statement.executeQuery();
 
